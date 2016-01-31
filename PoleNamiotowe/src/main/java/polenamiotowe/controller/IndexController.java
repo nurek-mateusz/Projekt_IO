ccf5f1dcbfb767d2a06f9c4c7e05d1421b22ecdb
Rammis <rammis94@gmail.com>
2016-01-31 17:58:30 +0100

    Rejestracja w bazie danych

--- PoleNamiotowe/src/main/java/polenamiotowe/controller/IndexController.java
+++ PoleNamiotowe/src/main/java/polenamiotowe/controller/IndexController.java
@@ -1,20 +1,30 @@
 package polenamiotowe.controller;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import repositories.*;
+
 @Controller
 public class IndexController {
+
+    UzytkownikRepository uzytkownikRespository;
     
-    
+    public IndexController(){
+        uzytkownikRespository = new UzytkownikRepository();
+    }
+
     @RequestMapping("/")
     public String index(Model model) {
         return "index";
     }
-    
+
     @RequestMapping("/kontakt")
     public ModelAndView kontakt(Model model) {
         ModelAndView mav = new ModelAndView();
@@ -22,22 +32,45 @@ public class IndexController {
         mav.addObject("parametr", 5);
         return mav;
     }
-    
-    @RequestMapping(value = "/rejestracja", method=RequestMethod.GET)
-        public ModelAndView rejestracjaGet(Model model, @RequestParam(value="pwd",required=false) String pwd) {
+
+    @RequestMapping(value = "/rejestracja", method = RequestMethod.GET)
+    public ModelAndView rejestracjaGet(Model model, @RequestParam(value = "pwd", required = false) String pwd, @RequestParam(value = "usr", required = false) String usr) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("rejestracja");
+
+        try {
+            if (!(pwd == null && usr == null)) {
+                if (uzytkownikRespository.UzytkownikIstnieje(usr, pwd)) {
+                    mav.addObject("blad", 1);
+                } else {
+                    uzytkownikRespository.RejestrujUzytkownika(usr, pwd);
+                }
+
+                return mav;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(IndexController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return mav;
+    }
+
+    @RequestMapping(value = "/rejestracja", method = RequestMethod.POST)
+    public ModelAndView rejestracjaPost(Model model, @RequestParam(value = "pwd", required = false) String pwd) {
+
         ModelAndView mav = new ModelAndView();
         mav.setViewName("rejestracja");
         return mav;
     }
-    
-    
-    @RequestMapping(value = "/rezerwacjaMiejsca", method=RequestMethod.GET)
+
+    @RequestMapping(value = "/rezerwacjaMiejsca", method = RequestMethod.GET)
     public ModelAndView rezerwacjaMiejscaGet(Model model) {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("RezerwacjaMiejsca");
         return mav;
     }
-        @RequestMapping(value = "/lista", method=RequestMethod.GET)
+
+    @RequestMapping(value = "/lista", method = RequestMethod.GET)
     public ModelAndView listaGet(Model model) {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("lista");
